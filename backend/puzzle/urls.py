4c3db6b1deb36fd198d533e5cfa5c1c5d5b5e5a6
mductran <ductran032118@gmail.com>
2021-09-27 14:06:49 +0700

    switch to vue

--- backend/puzzle/urls.py
+++ backend/puzzle/urls.py
@@ -1,18 +1,17 @@
-from backend.puzzle.views import image
 from django.urls import path, include
 from puzzle.views import *
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'puzzle', PuzzleView)
-router.register(r'account', AccountView)
-router.register(r'brand', BrandView)
-router.register(r'collage', CollageView)
-router.register(r'post', PostView)
-router.register(r'comment', CommentView)
-router.register(r'inventory', InventoryView)
-router.register(r'theme', ThemeView)
-router.register(r'image', ImageView)
+router.register(r'puzzles', PuzzleView)
+router.register(r'accounts', AccountView)
+router.register(r'brands', BrandView)
+router.register(r'collages', CollageView)
+router.register(r'posts', PostView)
+router.register(r'comments', CommentView)
+router.register(r'inventories', InventoryView)
+router.register(r'themes', ThemeView)
+router.register(r'images', ImageView)
 
 urlpatterns = [
     path('', include(router.urls)),
