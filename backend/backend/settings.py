eb50ec8ced1181881459ff58a1cdee3ba6aa1f86
mductran <ductran032118@gmail.com>
2022-02-17 11:01:35 +0700

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+    
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b83siyiudsazu!cls6184hu9(t4!ws4kqzeoq@td$n4f-s2_qr'
+# SECRET_KEY = 'django-insecure-b83siyiudsazu!cls6184hu9(t4!ws4kqzeoq@td$n4f-s2_qr'
+SECRET_KEY = os.environ["PUZZLE_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,7 +115,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'puzzle',
         'USER': 'padmin',
-        'PASSWORD': os.environ["MYSQL_PWD"],
+        'PASSWORD': os.environ["PUZZLE_RDS_PWD"],
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
